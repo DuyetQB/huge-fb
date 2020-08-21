fca6353fe9911f698d9c53a272173003d5816387
Dinhsiduyet <dinhsyduyet@outlook.com>
2020-08-21 15:21:58 +0700

    commit 1

---
+++ facebook.js
@@ -0,0 +1,4 @@
+//chào các bạn mình tên là Duyệt 
+//Mình thích lập trình web ,game ,app nhưng mỗi tội mấy cái đó khó quá 
+//nên mình quyết định thử thách tạo ra facebook 
+ //Nếu các bạn thấy hay thì hãy subcribe github của mình nhé hehe     
